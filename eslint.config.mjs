32647aee199acf04b87c9100aa663ff527ac9491
Skaiir <valentin.serra@camunda.com>
2025-12-08 15:58:09 +0100

    chore: migate to eslint@9.x
    Related to #64

---
+++ eslint.config.mjs
@@ -0,0 +1,107 @@
+import js from '@eslint/js';
+import bpmnPlugin from 'eslint-plugin-bpmn-io';
+import mochaPlugin from 'eslint-plugin-mocha';
+import reactPlugin from 'eslint-plugin-react';
+import globals from 'globals';
+
+const baseParserOptions = {
+  ecmaVersion: 2022,
+  sourceType: 'module'
+};
+
+const bpmnRules = bpmnPlugin.configs.recommended?.rules ?? {};
+const mochaRules = bpmnPlugin.configs.mocha?.rules ?? {};
+
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'feelers-playground/build/**',
+      'feelers-playground/dist/**',
+      'src/grammar/parser*.js',
+      '.github/**',
+      'karma.conf.js',
+      'eslint.config.mjs'
+    ]
+  },
+  js.configs.recommended,
+  {
+    files: [
+      'src/**/*.js',
+      'feelers-playground/src/**/*.js'
+    ],
+    languageOptions: {
+      parserOptions: {
+        ...baseParserOptions,
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser
+      }
+    },
+    plugins: {
+      'bpmn-io': bpmnPlugin,
+      react: reactPlugin
+    },
+    rules: {
+      ...bpmnRules,
+      'react/jsx-uses-vars': 'error',
+      'react/jsx-uses-react': 'off'
+    }
+  },
+  {
+    files: [
+      'rollup.config.js'
+    ],
+    languageOptions: {
+      parserOptions: {
+        ...baseParserOptions,
+        sourceType: 'commonjs'
+      },
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: [
+      'test/testBundle.js'
+    ],
+    languageOptions: {
+      parserOptions: {
+        ...baseParserOptions,
+        sourceType: 'commonjs'
+      },
+      globals: {
+        require: 'readonly'
+      }
+    }
+  },
+  {
+    files: [
+      'test/spec/**/*.js'
+    ],
+    languageOptions: {
+      parserOptions: {
+        ...baseParserOptions
+      },
+      globals: {
+        ...globals.mocha,
+        ...globals.browser,
+        sinon: 'readonly',
+        expect: 'readonly',
+        FeelEditor: 'readonly'
+      }
+    },
+    plugins: {
+      mocha: mochaPlugin
+    },
+    rules: {
+      ...mochaRules,
+      'no-unused-vars': 'off',
+      'no-undef': 'off'
+    }
+  }
+];
