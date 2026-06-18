dafcbe52fff07ddf11b846adb28e5e591fc482ca
Jarek Danielak <jarek.danielak@camunda.com>
2026-06-18 16:06:13 +0200

    chore: switch to `karma-tldr-reporter`

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function(karma) {
       [suite]: ['webpack', 'env']
     },
 
-    reporters: ['progress'].concat(coverage ? 'coverage' : []),
+    reporters: ['tldr'].concat(coverage ? 'coverage' : []),
 
     coverageReporter: {
       reporters: [
